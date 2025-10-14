66e1ff3029f25882f7c834717c4a70ba981b627c
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-10-14 18:44:37 +0200

    Obtention d'une blague aléatoire depuis une API externe en cliquant sur un bouton

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [joke, setjoke] = useState(null);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <button
+        type="button"
+        onClick={() => {
+          fetch("https://icanhazdadjoke.com/", {
+            headers: { Accept: "application/json" },
+          })
+            .then((res) => res.json())
+            .then((jokes) => {
+              setjoke(jokes.joke);
+            });
+        }}
+      >
+        Blague au hasard
+      </button>
+      <p>
+        {joke
+          ? joke
+          : "Cliquez sur le bouton pour obtenir une blague aléatoire"}
       </p>
     </>
   );
--- src/components/AddJokeForm.jsx
+++
@@ -1,5 +0,0 @@
-function AddJokeForm() {
-  return <h1>coucou</h1>;
-}
-
-export default AddJokeForm;
